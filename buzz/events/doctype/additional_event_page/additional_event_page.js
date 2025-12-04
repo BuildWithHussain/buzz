b3c2a121b81568fc70872db3d5fdba3afe5271dd
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-12-04 17:30:45 +0530

    refactor: load on refresh

--- buzz/events/doctype/additional_event_page/additional_event_page.js
+++ buzz/events/doctype/additional_event_page/additional_event_page.js
@@ -1,17 +1,11 @@
 // Copyright (c) 2025, BWH Studios and contributors
 // For license information, please see license.txt
-
-window.event_route = "";
-
 frappe.ui.form.on("Additional Event Page", {
-	onload(frm) {
-		frappe.db.get_value("Buzz Event", frm.doc.event, "route").then(({ message }) => {
-			window.event_route = message.route;
-		});
-	},
 	refresh(frm) {
 		if (frm.doc.is_published) {
-			frm.add_web_link(`/events/${window.event_route}/${frm.doc.route}`);
+			frappe.db.get_value("Buzz Event", frm.doc.event, "route").then(({ message }) => {
+				frm.add_web_link(`/events/${message.route}/${frm.doc.route}`);
+			});
 		}
 	},
 });
