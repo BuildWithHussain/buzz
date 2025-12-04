0093ae37803310b4705d3c24b34caa459d888a53
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-12-04 17:27:30 +0530

    feat: view on website in additional event page

--- buzz/events/doctype/additional_event_page/additional_event_page.js
+++ buzz/events/doctype/additional_event_page/additional_event_page.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2025, BWH Studios and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Additional Event Page", {
-// 	refresh(frm) {
+window.event_route = "";
 
-// 	},
-// });
+frappe.ui.form.on("Additional Event Page", {
+	onload(frm) {
+		frappe.db.get_value("Buzz Event", frm.doc.event, "route").then(({ message }) => {
+			window.event_route = message.route;
+		});
+	},
+	refresh(frm) {
+		if (frm.doc.is_published) {
+			frm.add_web_link(`/events/${window.event_route}/${frm.doc.route}`);
+		}
+	},
+});
