b08a056a1c79d6ace5ed3442fe953a8cf883745e
UmakanthKaspa <kaspaumakanth1999@gmail.com>
2025-12-31 10:08:33 +0000

    feat: add click to expand QR code in ticket views

---
+++ dashboard/src/components/QRCodeExpandDialog.vue
@@ -0,0 +1,36 @@
+<template>
+	<Dialog v-model="isOpen" :options="{ title: __('QR Code'), size: 'lg' }">
+		<template #body-content>
+			<div class="flex justify-center">
+				<img :src="qrCodeSrc" :alt="altText" class="w-full max-w-sm" />
+			</div>
+		</template>
+	</Dialog>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { Dialog } from "frappe-ui";
+
+const props = defineProps({
+	modelValue: {
+		type: Boolean,
+		default: false,
+	},
+	qrCodeSrc: {
+		type: String,
+		required: true,
+	},
+	altText: {
+		type: String,
+		default: "QR Code",
+	},
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const isOpen = computed({
+	get: () => props.modelValue,
+	set: (value) => emit("update:modelValue", value),
+});
+</script>
