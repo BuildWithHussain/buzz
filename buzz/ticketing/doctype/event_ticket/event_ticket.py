6e095d7a71ec4b0d9717f90459fcedf231202c10
Hussain Nagaria <hussainbhaitech@gmail.com>
2026-01-23 15:50:46 +0530

    refactor: utils for QR gen

--- buzz/ticketing/doctype/event_ticket/event_ticket.py
+++ buzz/ticketing/doctype/event_ticket/event_ticket.py
@@ -5,7 +5,7 @@ import frappe
 from frappe.core.api.user_invitation import invite_by_email
 from frappe.model.document import Document
 
-from buzz.utils import only_if_app_installed
+from buzz.utils import generate_qr_code_file, only_if_app_installed
 
 
 class EventTicket(Document):
@@ -130,18 +130,11 @@ class EventTicket(Document):
 			frappe.throw(frappe._("Coupon has been already used up maximum number of times!"))
 
 	def generate_qr_code(self):
-		qr_data = make_qr_image_with_data(f"{self.name}")
-		qr_code_file = frappe.get_doc(
-			{
-				"doctype": "File",
-				"content": qr_data,
-				"attached_to_doctype": "Event Ticket",
-				"attached_to_name": self.name,
-				"attached_to_field": "qr_code",
-				"file_name": f"ticket-qr-code-{self.name}.png",
-			}
-		).save(ignore_permissions=True)
-		self.qr_code = qr_code_file.file_url
+		self.qr_code = generate_qr_code_file(
+			doc=self,
+			data=self.name,
+			file_prefix="ticket-qr-code",
+		)
 
 	def on_cancel(self):
 		self.ignore_linked_doctypes = ["Event Booking", "Ticket Cancellation Request"]
@@ -157,25 +150,3 @@ class EventTicket(Document):
 			delayed=False,
 			retry=2,
 		)
-
-
-def make_qr_image_with_data(data: str) -> bytes:
-	import io
-
-	import qrcode
-	from qrcode.image.styledpil import StyledPilImage
-	from qrcode.image.styles.moduledrawers.pil import HorizontalBarsDrawer
-
-	qr = qrcode.QRCode(
-		version=1,
-		error_correction=qrcode.constants.ERROR_CORRECT_H,
-		box_size=10,
-		border=4,
-	)
-	qr.add_data(data)
-	qr.make(fit=True)
-
-	img = qr.make_image(image_factory=StyledPilImage, module_drawer=HorizontalBarsDrawer())
-	output = io.BytesIO()
-	img.save(output, format="PNG")
-	return output.getvalue()
