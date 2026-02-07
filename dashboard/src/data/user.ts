f1c98f7a30cfea8dadac290d49d4e870c0ff1bf3
Md Gulam Gaush <gulamgaush.dev@gmail.com>
2026-02-07 16:51:48 +0530

    chore: migrate data layer to TS

--- dashboard/src/data/user.ts
+++ dashboard/src/data/user.ts
@@ -1,9 +1,10 @@
 import { createResource } from "frappe-ui";
+import type { FrappeError } from "@/types/Frappe/FrappeError";
 
 export const userResource = createResource({
 	url: "buzz.api.get_user_info",
 	cache: "User",
-	onError(error) {
+	onError(error: FrappeError) {
 		if (error && error.exc_type === "AuthenticationError") {
 			window.location.href = "/login?redirect-to=dashboard";
 		}
